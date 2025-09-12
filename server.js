e664302d90dfe48d30ea7be272514b83615be6f7
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-12 17:01:37 +0530

    save cors changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import leadRoutes from "./routes/leadRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,17 @@ const app = express();
 
 connectDB();
 
+
+
+// Allow all origins
+app.use(cors());
+
+// If you also need to allow custom headers, methods, and credentials:
+app.use(cors({
+  origin: "*",             // allow all origins
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 // Middleware
 app.use(express.json());
 
