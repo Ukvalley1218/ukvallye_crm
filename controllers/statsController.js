33181b28eb415998a063a141cdfb12a562b73648
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-18 15:55:01 +0530

    remove protect from count

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -3,16 +3,18 @@ import Staff from "../models/Staff.js";
 import Customer from "../models/Customer.js";
 import Expense from "../models/Expense.js";
 import Project from "../models/Project.js";
+import Task from "../models/Tasks.js";
 
 export const getDashboardCounts = async (req, res, next) => {
   try {
-    const [leadCount, taskCount, staffCount, projectCount, customerCount] =
+    const [leadCount, taskCount, staffCount, projectCount, customerCount,expensecounts] =
       await Promise.all([
         Lead.countDocuments(),
         Task.countDocuments(),
         Staff.countDocuments(),
         Project.countDocuments(),
         Customer.countDocuments(),
+        Expense.countDocuments(),  
       ]);
 
     res.json({
@@ -21,6 +23,7 @@ export const getDashboardCounts = async (req, res, next) => {
       staff: staffCount,
       projects: projectCount,
       customers: customerCount,
+      expenses:expensecounts
     });
   } catch (err) {
     next(err);
