b15ee6b1bee7b1cc08b6b2cdd63bdced8b5a2c56
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-18 15:49:33 +0530

    remove protect from count

--- routes/statRoutes.js
+++ routes/statRoutes.js
@@ -3,6 +3,6 @@ import { getDashboardCounts } from '../controllers/statsController.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-router.get('/',protect, getDashboardCounts);
+router.get('/', getDashboardCounts);
 
 export default router;
