95a0182a49d30693ec07a0aba1963f872eaf8401
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-25 15:08:55 +0530

    save changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,13 +5,15 @@ import User from "../models/User.js";
 // Verify JWT
 export const protect = async (req, res, next) => {
   let token;
-
-  if (req.headers.authorization?.startsWith("Bearer")) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.id).select("-password");
+console.log(req.headers.authorization); // should print "Bearer <token>"
+if (req.headers.authorization?.startsWith("Bearer")) {
+  try {
+    token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    req.user = await User.findById(decoded.id).select("-password");
+    console.log(req.user);                  // should show the logged-in user
+      
       next();
     } catch (error) {
       return res.status(401).json({ message: "Not authorized, token failed" });
